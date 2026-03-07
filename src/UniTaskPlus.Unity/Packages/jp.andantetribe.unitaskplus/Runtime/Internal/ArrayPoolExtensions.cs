4d9e3ccc526049137112dc4fcf6f39621c1f6f5d
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-03-07 13:47:07 +0900

    feat: Add initial project files and configuration for UniTask Plus

---
+++ src/UniTaskPlus.Unity/Packages/jp.andantetribe.unitaskplus/Runtime/Internal/ArrayPoolExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace UniTaskPlus.Internal
+{
+    internal static class ArrayPoolExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Grow<T>(this ArrayPool<T> pool, ref T[] array, int minimumLength)
+        {
+            if (array.Length < minimumLength)
+            {
+                var newArray = pool.Rent(minimumLength);
+                if (array.Length > 0)
+                {
+                    var temp = array.AsSpan();
+                    temp.CopyTo(newArray);
+                    temp.Clear();
+                    pool.Return(array);
+                }
+                array = newArray;
+            }
+        }
+    }
+}
---
+++ src/UniTaskPlus.Unity/Packages/jp.andantetribe.unitaskplus/Runtime/UniTaskBag.cs
@@ -0,0 +1,91 @@
+﻿#nullable enable
+
+using System;
+using System.Buffers;
+using Cysharp.Threading.Tasks;
+using UniTaskPlus.Internal;
+
+namespace UniTaskPlus
+{
+    /// <summary>
+    /// A bag that collects multiple <see cref="UniTask"/> and completes when all of them are completed.
+    /// </summary>
+    /// <remarks>
+    /// The behavior of concurrently waiting for multiple <see cref="UniTask"/> is the same as <see cref="UniTask.WhenAll(UniTask[])"/>.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// using System;
+    /// using AndanteTribe.Utils.Unity;
+    /// using Cysharp.Threading.Tasks;
+    /// using UnityEngine;
+    ///
+    /// public class Example : MonoBehaviour
+    /// {
+    ///     private async UniTaskVoid Start()
+    ///     {
+    ///         // Using a list
+    ///         // var list = new List<UniTask>();
+    ///         // for (int i = 0; i < 10; i++)
+    ///         // {
+    ///         //     list.Add(UniTask.Delay(TimeSpan.FromSeconds(i)));
+    ///         // }
+    ///         // await UniTask.WhenAll(list);
+    ///
+    ///         // Using UniTaskBag
+    ///         await using (var bag = new UniTaskBag())
+    ///         {
+    ///             for (int i = 0; i < 10; i++)
+    ///             {
+    ///                 bag.Add(UniTask.Delay(TimeSpan.FromSeconds(i)));
+    ///             }
+    ///         }
+    ///     }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    public struct UniTaskBag : IUniTaskAsyncDisposable
+    {
+        private UniTask[]? _tasks;
+        private int _count;
+
+        /// <summary>
+        /// Adds a <see cref="UniTask"/> to the bag.
+        /// </summary>
+        /// <param name="task">The <see cref="UniTask"/> to add.</param>
+        public void Add(UniTask task)
+        {
+            if (_tasks == null)
+            {
+                _tasks = ArrayPool<UniTask>.Shared.Rent(1);
+            }
+            else
+            {
+                ArrayPool<UniTask>.Shared.Grow(ref _tasks, _count + 1);
+            }
+
+            _tasks[_count++] = task;
+        }
+
+        /// <inheritdoc />
+        public UniTask DisposeAsync()
+        {
+            if (_tasks == null)
+            {
+                return UniTask.CompletedTask;
+            }
+
+            try
+            {
+                return UniTask.WhenAll(_tasks);
+            }
+            finally
+            {
+                _tasks.AsSpan(0, _count).Clear();
+                ArrayPool<UniTask>.Shared.Return(_tasks);
+            }
+        }
+    }
+}
